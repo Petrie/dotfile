a7b91c788cdc7c20b39382a7db679f7220ed0a87
liupengfei <liupengfei@inke.cn>
2022-05-21 11:18:00 +0800

    feat(更新init): 更新init脚本

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 ln -s $HOME/dotfile/.vimrc  $HOME/.vimrc
 ln -s $HOME/dotfile/.golangci.yml $HOME/.golangci.yml
 ln -s $HOME/dotfile/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfile/.gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfile/.gitignore $HOME/.gitignore
